97d5d3346988a3db02ed5f28aa0f58a2331e2728
Ramesh P <rameshpedada@gmail.com>
2019-09-17 06:44:47 +0000

    change for scm poll

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 class helloWorld {
  public static void main(String args[]){
      System.out.println("Hello World");
-     System.out.println("Poll SCM working");
+     System.out.println("Poll SCM worked");
  }
 }
